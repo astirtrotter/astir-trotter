c8fc9a8568b14e54b40c6a917a9136c94850d60b
Pro Dev <prodev9999@gmail.com>
2019-03-12 09:56:53 -0700

    Updated project structure

--- ATWeb/app.js
+++ ATWeb/app.js
@@ -7,25 +7,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var frontendRoutes = require('./routes/frontend/index');
+var backendRoutes = require('./routes/backend/index');
 var app = express();
+
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// frontend
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use('/', frontendRoutes);
+
+// backend
+app.use('/api', backendRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -61,5 +64,5 @@ app.use(function (err, req, res, next) {
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+    debug('Server is running on port ' + server.address().port);
 });
---
+++ ATWeb/routes/backend/index.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+// check if this is valid request
+router.use(function (req, res, next) {
+    if (req.body.ver === process.env.VERSION) {
+        next();
+    } else {
+        res.status(301);
+        res.send({
+            success: false,
+            error: {
+                message: 'The version of the client is old. Please update and try again.'
+            }
+        })
+    }
+});
+
+router.get('/', function (req, res) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
