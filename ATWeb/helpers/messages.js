14bcb93a58a908908f05287944fd27f2333c516c
Pro Dev <prodev9999@gmail.com>
2019-03-14 12:46:49 +0900

    Create messages.js

---
+++ ATWeb/helpers/messages.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+module.exports = {
+    Warning: {
+
+    },
+    Error: {
+        Version: 'The version of the client you are using is incorrect. Please update and try again.'
+    }
+};
