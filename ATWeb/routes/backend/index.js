33daef120fb776ff049d634ef31a9596d83251b4
Pro Dev <prodev9999@gmail.com>
2019-03-14 12:47:28 +0900

    Updated Backend middleware

--- ATWeb/routes/backend/index.js
+++ ATWeb/routes/backend/index.js
@@ -1,30 +1,31 @@
 ﻿'use strict';
+var Messages = require('../../helpers/messages');
 
 module.exports = function (app) {
     /* middleware */
 
     // check version
-    app.use(function (req, res, next) {
-        if (!req.url.startsWith(process.env.API_BASE_URL) ||
-            req.url.startsWith(process.env.API_BASE_URL + process.env.VERSION + '/')) {
+    app.all(process.env.API_BASE_URL + '*', function (req, res, next) {
+        if (req.url.startsWith(process.env.API_BASE_URL + process.env.VERSION + '/')) {
             return next();
         } else {
             res.status(301);
             return res.send({
                 success: false,
                 error: {
-                    message: 'The version of the client you are using is old. Please update and try again.'
+                    message: Messages.Error.Version
                 }
             });
         }
     });
 
     app.param('userId', function (req, res, next, userId) {
-
+        var User = require('../../models/user');
+        req.user = User.getUserById(userId);
+        next();
     });
     
-    app.get(process.env.API_BASE_URL + process.env.VERSION +
-        '/', function (req, res, next) {
-        
+    app.get(process.env.API_BASE_URL + process.env.VERSION + '/', function (req, res, next) {
+        res.send('abc');
     });
 };
