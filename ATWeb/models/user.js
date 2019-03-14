2e678b9e25c1f0d05a31d43b2add39b805cfd6f8
Pro Dev <prodev9999@gmail.com>
2019-03-14 16:46:49 +0900

    Create user.js

---
+++ ATWeb/models/user.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var db = require('../helpers/msSqlHelper');
+var TYPES = require("tedious").TYPES;
+
+var user = {
+    getUserById: function (userId) {
+        var ret;
+        db.query("Users", "UserId='" + userId + "'", function (count, data) {
+            ret = data;
+        });
+
+        return ret;
+    }
+};
+
+module.exports = user;
