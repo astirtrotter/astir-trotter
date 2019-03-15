c05138831f5f7046e27b4ef0b13f957c3f2188f5
Pro Dev <prodev9999@gmail.com>
2019-03-15 19:03:46 +0900

    Minor project environment update

--- ATWeb/routes/index.js
+++ ATWeb/routes/index.js
@@ -2,12 +2,18 @@
 
 module.exports = function (app) {
 
+    process.env.API_URL = process.env.API_BASE_URL + process.env.API_VERSION;
+
     // middleware
     require('../middleware/api_versioning')(app);
     require('../middleware/api_params')(app);
 
     // routes
-    app.get(process.env.API_BASE_URL + process.env.API_VERSION + '/:userId', function (req, res, next) {
-        res.send(req.user);
+    app.get(process.env.API_URL + '/:userId', function (req, res, next) {
+        if (req.user) {
+            res.send(req.user);
+        } else {
+            res.send("This user doesn't exist.");
+        }
     });
 };
