432df0ad383cc6221282bb5b94ef96f5b0e04edb
Pro Dev <prodev9999@gmail.com>
2019-03-17 01:25:14 +0900

    Added MVC pattern base classes

---
+++ ATDesktopWin/Controllers/_Controller.cs
@@ -0,0 +1,47 @@
+﻿using ATDesktopWin.Views;
+using System;
+using System.Windows.Forms;
+
+
+namespace ATDesktopWin.Controllers
+{
+    public abstract class _Controller
+    {
+        public abstract IView View { get; }
+
+        public event EventHandler LoadSuccess, LoadFailed;
+
+        public _Controller()
+        {
+            LoadSuccess += OnLoadSucceededHandler;
+            LoadFailed += OnLoadFailedHandler;
+        }
+
+        public abstract bool Loadable();
+
+        protected virtual void OnLoadSucceededHandler(Object sender, EventArgs e)
+        {
+            AppManager.Instance.Show(this);
+        }
+
+        protected virtual void OnLoadFailedHandler(Object sender, EventArgs e)
+        {
+            if (MessageBox.Show("You don't have access to load this page.\nThe application will exit now.") == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        public void OnLoadSuccess(EventArgs e)
+        {
+            if (LoadSuccess != null)
+                LoadSuccess(this, e);
+        }
+
+        public void OnLoadFailure(EventArgs e)
+        {
+            if (LoadFailed != null)
+                LoadFailed(this, e);
+        }
+    }
+}
---
+++ ATDesktopWin/Views/IView.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace ATDesktopWin.Views
+{
+    public interface IView
+    {
+        string Title { get; set; }
+
+        Form Form { get; }
+
+        void Close();
+    }
+}
