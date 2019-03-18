9d56761f75b72c10b60364abc8bbcbddc5e1d063
Pro Dev <prodev9999@gmail.com>
2019-03-19 00:20:24 +0900

    Added credentials helper

---
+++ ATWeb/helpers/credentialsHelper.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+const Crypto = require('crypto');
+const Sha256 = require('sha256');
+
+module.exports = {
+    genToken: function () {
+        const buf = Crypto.randomBytes(48);
+        if (buf) {
+            return buf.toString('hex');
+        } else {
+            return null;
+        }
+    },
+    genPwdHash: function (pwd) {
+        return Sha256.x2(pwd);
+    }
+};
