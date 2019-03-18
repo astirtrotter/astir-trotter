e9839186a224c89ab07d1fcbfea7cc7d1b66be00
Pro Dev <prodev9999@gmail.com>
2019-03-18 16:46:13 +0900

    Added ATCommon project

---
+++ ATCommon/API/ClientManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ATCommon.API
+{
+    public class ClientManager
+    {
+        private const string BASE_URL = "http://localhost:8888";
+        private const string API_URL = "/api/v1";
+        
+        private static ClientManager _instance;
+        public static ClientManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ClientManager();
+                }
+                return _instance;
+            }
+        }
+
+        private RestClient client;
+
+        private ClientManager()
+        {
+            client = new RestClient(BASE_URL + API_URL);
+        }
+
+    }
+}
