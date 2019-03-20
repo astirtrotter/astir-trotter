5695b994bd0e5a01d24da85f9dcf527607c49610
Pro Dev <prodev9999@gmail.com>
2019-03-20 07:13:43 -0700

    Update api_params.js

--- ATWeb/middleware/api_params.js
+++ ATWeb/middleware/api_params.js
@@ -4,9 +4,18 @@ const models = require('../models');
 
 module.exports = function (app) {
 
+    // userId
     app.param('userId', function (req, res, next, userId) {
         models.User.scope({ method: ['byId', userId] }).findOne().then(user => {
             req.user = user;
+            next();
+        });
+    });
+
+    // anotherUserId
+    app.param('anotherUserId', function (req, res, next, anotherUserId) {
+        models.User.scope({ method: ['byId', anotherUserId] }).findOne().then(user => {
+            req.anotherUser = user;
             next();
         });
     });
