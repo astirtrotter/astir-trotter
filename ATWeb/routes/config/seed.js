00b1801aa9d11136b173c4df6f86812104bd83e2
Pro Dev <prodev9999@gmail.com>
2019-03-20 08:55:43 -0700

    Update seed.js

--- ATWeb/routes/config/seed.js
+++ ATWeb/routes/config/seed.js
@@ -3,13 +3,11 @@
 module.exports = function (req, res, next) {
     res.send({
         success: true,
-        seed: {
-            gender: require('../../config/seed/_gender'),
-            party: require('../../config/seed/_party'),
-            nationality: require('../../config/seed/_nationality'),
-            classOrigin: require('../../config/seed/_origin-class'),
-            socialOrigin: require('../../config/seed/_origin-social'),
-            nationalOrigin: require('../../config/seed/_origin-national')
-        }
+        genders: require('../../config/seed/_gender'),
+        parties: require('../../config/seed/_party'),
+        nationalities: require('../../config/seed/_nationality'),
+        classOrigins: require('../../config/seed/_origin-class'),
+        socialOrigins: require('../../config/seed/_origin-social'),
+        nationalOrigins: require('../../config/seed/_origin-national')
     });
 };
