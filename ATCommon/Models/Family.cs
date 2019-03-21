c3b658b5876fb8c0dde8476a0f6d6c73c49d375e
Pro Dev <prodev9999@gmail.com>
2019-03-21 02:32:33 -0700

    Added family model

---
+++ ATCommon/Models/Family.cs
@@ -0,0 +1,15 @@
+﻿using ATCommon.Models;
+using System.Collections.Generic;
+
+namespace ATCommon.Models
+{
+    public class Family
+    {
+        public User user { get; set; }
+        public Family spouse { get; set; }
+        public Family father { get; set; }
+        public Family mother { get; set; }
+        public List<Family> siblings { get; set; }
+        public List<Family> children { get; set; }
+    }
+}
---
+++ ATCommon/Responses/FamilyResponse.cs
@@ -0,0 +1,9 @@
+﻿using ATCommon.Models;
+
+namespace ATCommon.Responses
+{
+    public class FamilyResponse : _Response
+    {
+        public Family family { get; set; }
+    }
+}
