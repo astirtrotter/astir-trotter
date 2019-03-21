58a81b6f5dcd37373afa34e6b7acdc4872f35b2c
Pro Dev <prodev9999@gmail.com>
2019-03-21 02:31:59 -0700

    Added get-family API call in client

---
+++ ATCommon/API/ClientManager_users_family.cs
@@ -0,0 +1,18 @@
+﻿using ATCommon.Responses;
+using RestSharp;
+using System;
+
+namespace ATCommon.API
+{
+    public partial class ClientManager
+    {
+        public void GetFamily(string userId, Action<FamilyResponse> callback)
+        {
+            RestRequest request = new RestRequest("users/:userId/family", Method.GET);
+            request.AddUrlSegment("userId", userId);
+            client.ExecuteAsync<FamilyResponse>(request, res => {
+                callback.Invoke(res.Data);
+            });
+        }
+    }
+}
