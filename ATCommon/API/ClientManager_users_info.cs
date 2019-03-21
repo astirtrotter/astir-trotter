d5338cecf6f73566469059786c084a40554a6972
Pro Dev <prodev9999@gmail.com>
2019-03-21 02:32:16 -0700

    Added get-info API call in client

---
+++ ATCommon/API/ClientManager_users_info.cs
@@ -0,0 +1,18 @@
+﻿using ATCommon.Responses;
+using RestSharp;
+using System;
+
+namespace ATCommon.API
+{
+    public partial class ClientManager
+    {
+        public void GetUser(string userId, Action<UserResponse> callback)
+        {
+            RestRequest request = new RestRequest("users/:userId", Method.GET);
+            request.AddUrlSegment("userId", userId);
+            client.ExecuteAsync<UserResponse>(request, res => {
+                callback.Invoke(res.Data);
+            });
+        }
+    }
+}
