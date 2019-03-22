496948fc460b61e294fade34b5e6035dc39ae844
Pro Dev <prodev9999@gmail.com>
2019-03-22 09:14:01 -0700

    Implemented uploading profile picture

--- ATCommon/API/ClientManager_auth_signup.cs
+++ ATCommon/API/ClientManager_auth_signup.cs
@@ -13,35 +13,53 @@ namespace ATCommon.API
             string classOrigin, string socialOrigin, string phoneNumber, string email,
             string details, string diedDay, byte[] picture, Action<LoginResponse, string> callback)
         {
-            IRestRequest request = new RestRequest("auth/signup", Method.POST)
-                //.AddParameter("userId", userId, ParameterType.RequestBody)
-                //.AddParameter("password", password, ParameterType.RequestBody)
-                //.AddParameter("firstName", firstName, ParameterType.RequestBody)
-                //.AddParameter("lastName", lastName, ParameterType.RequestBody)
-                //.AddParameter("gender", gender, ParameterType.RequestBody)
-                //.AddParameter("birthday", birthday, ParameterType.RequestBody);
-                .AddJsonBody(new
-                 {
-                     userId = userId,
-                     password = password,
-                     firstName = firstName,
-                     lastName = lastName,
-                     nickName = nickName,
-                     gender = gender,
-                     birthday = birthday,
-                     address = address,
-                     birthPlace = birthPlace,
-                     party = party,
-                     nationality = nationality,
-                     nationalOrigin = nationalOrigin,
-                     classOrigin = classOrigin,
-                     socialOrigin = socialOrigin,
-                     phoneNumber = phoneNumber,
-                     email = email,
-                     details = details,
-                     diedDay = diedDay
-                 });
-            //.AddFile("picture", @"D:\Work\remote\mine\AstirTrotter\ATCommon\API\ClientManager_auth_signup.cs");
+            IRestRequest request = new RestRequest("auth/signup", Method.POST);
+            if (picture != null)
+            {
+                request.AddHeader("Content-Type", "multipart/form-data")
+                    .AddParameter("userId", userId)
+                    .AddParameter("password", password)
+                    .AddParameter("firstName", firstName)
+                    .AddParameter("lastName", lastName)
+                    .AddParameter("gender", gender)
+                    .AddParameter("birthday", birthday)
+                    .AddParameter("address", address)
+                    .AddParameter("birthPlace", birthPlace)
+                    .AddParameter("party", party)
+                    .AddParameter("nationality", nationality)
+                    .AddParameter("classOrigin", classOrigin)
+                    .AddParameter("socialOrigin", socialOrigin)
+                    .AddParameter("nationalOrigin", nationalOrigin)
+                    .AddParameter("phoneNumber", phoneNumber)
+                    .AddParameter("email", email)
+                    .AddParameter("details", details)
+                    .AddParameter("diedDay", diedDay)
+                    .AddFileBytes("picture", picture, "profile-picture-" + userId + ".png");
+            } else
+            {
+                request.AddJsonBody(new
+                    {
+                        userId = userId,
+                        password = password,
+                        firstName = firstName,
+                        lastName = lastName,
+                        nickName = nickName,
+                        gender = gender,
+                        birthday = birthday,
+                        address = address,
+                        birthPlace = birthPlace,
+                        party = party,
+                        nationality = nationality,
+                        nationalOrigin = nationalOrigin,
+                        classOrigin = classOrigin,
+                        socialOrigin = socialOrigin,
+                        phoneNumber = phoneNumber,
+                        email = email,
+                        details = details,
+                        diedDay = diedDay
+                    });
+            }
+            
             client.ExecuteAsync<LoginResponse>(request, res => {
                 callback.Invoke(res.Data, GetErrorMessage(res));
             });
