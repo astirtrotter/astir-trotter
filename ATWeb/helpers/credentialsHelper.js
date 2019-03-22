4c9230d46ef3ce13a4883040483f295135d6c8b0
Pro Dev <prodev9999@gmail.com>
2019-03-22 00:42:18 -0700

    Update credentialsHelper.js

--- ATWeb/helpers/credentialsHelper.js
+++ ATWeb/helpers/credentialsHelper.js
@@ -1,16 +1,16 @@
 ﻿'use strict';
-const Crypto = require('crypto');
 const Sha256 = require('sha256');
 const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-    genToken: function () {
-        const buf = Crypto.randomBytes(48);
-        if (buf) {
-            return buf.toString('hex');
-        } else {
-            return null;
-        }
+    genToken: function (userId) {
+        return jwt.sign({ userId: userId, extra: process.hrtime() }, process.env.TOKEN_SIGN_SECURITY, { expiresIn: process.env.TOKEN_EXPIRES_IN });
+    },
+    verifyToken: function (token, callback) {
+        jwt.verify(token, process.env.TOKEN_SIGN_SECURITY, function (err, decoded) {
+            callback(err, decoded);
+        });
     },
     genPwdHash: function (pwd) {
         return Sha256.x2(pwd);
