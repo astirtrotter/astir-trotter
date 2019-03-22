478d3600bc61ea6a1e3909d4acabb36c2b90dceb
Pro Dev <prodev9999@gmail.com>
2019-03-22 00:42:23 -0700

    Update messages.js

--- ATWeb/config/messages.js
+++ ATWeb/config/messages.js
@@ -16,10 +16,10 @@ module.exports = {
     },
     Error: {
         NotFound: 'Not Found',
+        InvalidToken: 'Invalid Token',
         Version: 'The version of the client you are using is incorrect. You should update your client app first.',
         NoSetting: 'User settings had not been stored correctly. Please contact admin.',
         NoFamily: 'User family data had not been stored correctly. Please contact admin.',
-        GenToken: 'Cannot generate a token. Please contact admin.',
         SaveData: 'Cannot save token of new login. Please contact admin.'
     }
 };
