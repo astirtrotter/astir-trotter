96aaa56f05f9c9ccaa3cd25416bfd5ac7a792193
Pro Dev <prodev9999@gmail.com>
2019-03-22 01:06:31 -0700

    Update log.js

--- ATWeb/middleware/log.js
+++ ATWeb/middleware/log.js
@@ -16,40 +16,18 @@ module.exports = function (app) {
             loc = 'Local';
         }
 
-        const token = req.body.token;
-        if (token) {
-            models.Token.scope({ method: ['byToken', token] }).findOne().then(tokenInfo => {
-                models.Log.create({
-                    method: req.method,
-                    reqUrl: req.url,
-                    reqHeaders: JSON.stringify(req.headers),
-                    reqBody: JSON.stringify(req.body),
-                    callerIp: ip,
-                    callerLocation: loc,
-                    token: token,
-                    userId: tokenInfo.UserId
-                }).then(function (data) {
-                    res.status(200);
-                    res.json(data.get({ plain: true }));
-                }).catch(function (error) {
-                    res.status(500);
-                    res.json({ error: error, stackError: error.stack });
-                });
-            });
-        } else {
-            models.Log.create({
-                method: req.method,
-                reqUrl: req.url,
-                reqHeaders: JSON.stringify(req.headers),
-                reqBody: JSON.stringify(req.body),
-                callerIp: ip,
-                callerLocation: loc
-            }).then(function (data) {
-                console.log(data.get({ plain: true }));
-            }).catch(function (error) {
-                console.log(error.message);
-            });
-        }
+        models.Log.create({
+            method: req.method,
+            reqUrl: req.url,
+            reqHeaders: JSON.stringify(req.headers),
+            reqBody: JSON.stringify(req.body),
+            callerIp: ip,
+            callerLocation: loc
+        }).then(function (data) {
+            console.log(data.get({ plain: true }));
+        }).catch(function (error) {
+            console.log(error.message);
+        });
 
         next();
     });
