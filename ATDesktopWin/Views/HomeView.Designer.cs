1a6b8c2e2e88349676fa2d12ac5146f9b0879915
Pro Dev <prodev9999@gmail.com>
2019-03-25 00:31:18 -0700

    Designed HomeView with several pages

--- ATDesktopWin/Views/HomeView.Designer.cs
+++ ATDesktopWin/Views/HomeView.Designer.cs
@@ -28,28 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TabPage tpDashboard;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeView));
             this.menuMain = new System.Windows.Forms.MenuStrip();
             this.appToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.screenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.screenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.familyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.discoverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.messagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.albumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.treeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.websiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.feedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scMain = new System.Windows.Forms.SplitContainer();
+            this.tcPages = new System.Windows.Forms.TabControl();
+            this.tpFamilyTree = new System.Windows.Forms.TabPage();
+            this.tpFamilyTable = new System.Windows.Forms.TabPage();
+            tpDashboard = new System.Windows.Forms.TabPage();
             this.menuMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scMain)).BeginInit();
+            this.scMain.Panel2.SuspendLayout();
+            this.scMain.SuspendLayout();
+            this.tcPages.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tpDashboard
+            // 
+            tpDashboard.Location = new System.Drawing.Point(4, 29);
+            tpDashboard.Name = "tpDashboard";
+            tpDashboard.Padding = new System.Windows.Forms.Padding(3);
+            tpDashboard.Size = new System.Drawing.Size(1000, 608);
+            tpDashboard.TabIndex = 0;
+            tpDashboard.Text = "Dashboard";
+            tpDashboard.UseVisualStyleBackColor = true;
             // 
             // menuMain
             // 
@@ -74,6 +94,23 @@
             this.appToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.appToolStripMenuItem.Text = "&App";
             // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.refreshToolStripMenuItem.Text = "&Refresh";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(110, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
             // screenToolStripMenuItem
             // 
             this.screenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -86,6 +123,51 @@
             this.screenToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
             this.screenToolStripMenuItem.Text = "&Screen";
             // 
+            // dashboardToolStripMenuItem
+            // 
+            this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
+            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.dashboardToolStripMenuItem.Text = "&Dashboard";
+            // 
+            // familyToolStripMenuItem
+            // 
+            this.familyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tableToolStripMenuItem,
+            this.treeToolStripMenuItem});
+            this.familyToolStripMenuItem.Name = "familyToolStripMenuItem";
+            this.familyToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.familyToolStripMenuItem.Text = "&Family";
+            // 
+            // tableToolStripMenuItem
+            // 
+            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
+            this.tableToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.tableToolStripMenuItem.Text = "Table";
+            // 
+            // treeToolStripMenuItem
+            // 
+            this.treeToolStripMenuItem.Name = "treeToolStripMenuItem";
+            this.treeToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.treeToolStripMenuItem.Text = "Tree";
+            // 
+            // discoverToolStripMenuItem
+            // 
+            this.discoverToolStripMenuItem.Name = "discoverToolStripMenuItem";
+            this.discoverToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.discoverToolStripMenuItem.Text = "D&iscover";
+            // 
+            // messagesToolStripMenuItem
+            // 
+            this.messagesToolStripMenuItem.Name = "messagesToolStripMenuItem";
+            this.messagesToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.messagesToolStripMenuItem.Text = "&Messages";
+            // 
+            // albumToolStripMenuItem
+            // 
+            this.albumToolStripMenuItem.Name = "albumToolStripMenuItem";
+            this.albumToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.albumToolStripMenuItem.Text = "&Album";
+            // 
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -93,6 +175,12 @@
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
             this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.settingsToolStripMenuItem.Text = "S&ettings";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem.Text = "&Options";
             // 
             // helpToolStripMenuItem
             // 
@@ -104,105 +192,98 @@
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "&Help";
             // 
-            // refreshToolStripMenuItem
-            // 
-            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
-            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.refreshToolStripMenuItem.Text = "&Refresh";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "E&xit";
-            // 
-            // dashboardToolStripMenuItem
-            // 
-            this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
-            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.dashboardToolStripMenuItem.Text = "&Dashboard";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
-            // 
-            // familyToolStripMenuItem
-            // 
-            this.familyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tableToolStripMenuItem,
-            this.treeToolStripMenuItem});
-            this.familyToolStripMenuItem.Name = "familyToolStripMenuItem";
-            this.familyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.familyToolStripMenuItem.Text = "&Family";
-            // 
-            // discoverToolStripMenuItem
-            // 
-            this.discoverToolStripMenuItem.Name = "discoverToolStripMenuItem";
-            this.discoverToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.discoverToolStripMenuItem.Text = "D&iscover";
-            // 
-            // messagesToolStripMenuItem
-            // 
-            this.messagesToolStripMenuItem.Name = "messagesToolStripMenuItem";
-            this.messagesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.messagesToolStripMenuItem.Text = "&Messages";
-            // 
-            // albumToolStripMenuItem
-            // 
-            this.albumToolStripMenuItem.Name = "albumToolStripMenuItem";
-            this.albumToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.albumToolStripMenuItem.Text = "&Album";
-            // 
-            // tableToolStripMenuItem
-            // 
-            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
-            this.tableToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.tableToolStripMenuItem.Text = "Table";
-            // 
-            // treeToolStripMenuItem
-            // 
-            this.treeToolStripMenuItem.Name = "treeToolStripMenuItem";
-            this.treeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.treeToolStripMenuItem.Text = "Tree";
-            // 
-            // optionsToolStripMenuItem
-            // 
-            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.optionsToolStripMenuItem.Text = "&Options";
-            // 
             // websiteToolStripMenuItem
             // 
             this.websiteToolStripMenuItem.Name = "websiteToolStripMenuItem";
-            this.websiteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.websiteToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.websiteToolStripMenuItem.Text = "Website";
             // 
             // feedbackToolStripMenuItem
             // 
             this.feedbackToolStripMenuItem.Name = "feedbackToolStripMenuItem";
-            this.feedbackToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.feedbackToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.feedbackToolStripMenuItem.Text = "Feedback";
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // scMain
+            // 
+            this.scMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.scMain.IsSplitterFixed = true;
+            this.scMain.Location = new System.Drawing.Point(0, 24);
+            this.scMain.Name = "scMain";
+            this.scMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // scMain.Panel1
+            // 
+            this.scMain.Panel1.BackColor = System.Drawing.SystemColors.Info;
+            // 
+            // scMain.Panel2
+            // 
+            this.scMain.Panel2.Controls.Add(this.tcPages);
+            this.scMain.Size = new System.Drawing.Size(1008, 705);
+            this.scMain.SplitterDistance = 60;
+            this.scMain.TabIndex = 1;
+            // 
+            // tcPages
+            // 
+            this.tcPages.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tcPages.Controls.Add(tpDashboard);
+            this.tcPages.Controls.Add(this.tpFamilyTree);
+            this.tcPages.Controls.Add(this.tpFamilyTable);
+            this.tcPages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcPages.ItemSize = new System.Drawing.Size(50, 25);
+            this.tcPages.Location = new System.Drawing.Point(0, 0);
+            this.tcPages.Multiline = true;
+            this.tcPages.Name = "tcPages";
+            this.tcPages.SelectedIndex = 0;
+            this.tcPages.Size = new System.Drawing.Size(1008, 641);
+            this.tcPages.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tcPages.TabIndex = 2;
+            // 
+            // tpFamilyTree
+            // 
+            this.tpFamilyTree.Location = new System.Drawing.Point(4, 29);
+            this.tpFamilyTree.Name = "tpFamilyTree";
+            this.tpFamilyTree.Padding = new System.Windows.Forms.Padding(3);
+            this.tpFamilyTree.Size = new System.Drawing.Size(1000, 608);
+            this.tpFamilyTree.TabIndex = 1;
+            this.tpFamilyTree.Text = "FamilyTree";
+            this.tpFamilyTree.UseVisualStyleBackColor = true;
+            // 
+            // tpFamilyTable
+            // 
+            this.tpFamilyTable.Location = new System.Drawing.Point(4, 29);
+            this.tpFamilyTable.Name = "tpFamilyTable";
+            this.tpFamilyTable.Size = new System.Drawing.Size(1000, 608);
+            this.tpFamilyTable.TabIndex = 2;
+            this.tpFamilyTable.Text = "FamilyTable";
+            this.tpFamilyTable.UseVisualStyleBackColor = true;
             // 
             // HomeView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.scMain);
             this.Controls.Add(this.menuMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuMain;
             this.MinimumSize = new System.Drawing.Size(1024, 768);
             this.Name = "HomeView";
             this.Text = "Astir Trotter (Home)";
+            this.Shown += new System.EventHandler(this.HomeView_Shown);
             this.menuMain.ResumeLayout(false);
             this.menuMain.PerformLayout();
+            this.scMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scMain)).EndInit();
+            this.scMain.ResumeLayout(false);
+            this.tcPages.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -229,5 +310,9 @@
         private System.Windows.Forms.ToolStripMenuItem websiteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem feedbackToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer scMain;
+        private System.Windows.Forms.TabControl tcPages;
+        private System.Windows.Forms.TabPage tpFamilyTree;
+        private System.Windows.Forms.TabPage tpFamilyTable;
     }
 }
