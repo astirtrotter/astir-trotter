100e1b1dec2481c1a3df71387c51da08b356f883
Pro Dev <prodev9999@gmail.com>
2019-03-25 00:32:34 -0700

    Created Dashboard page controller

---
+++ ATDesktopWin/Controllers/PageControllers/DashboardPageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATDesktopWin.Controllers.PageControllers
+{
+    public class DashboardPageController : _PageController
+    {
+        public DashboardPageController(_Controller parent) : base(parent)
+        {
+            
+        }
+
+        protected override Object PageData {
+            get => null;
+        }
+
+        protected override void LoadPageData(Action actionOnSuccess)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RepresentPageData()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ShowPage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
