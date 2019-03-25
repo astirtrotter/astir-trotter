d56cd85d94d9c349d50b4ee15e745d6befa9d37e
Pro Dev <prodev9999@gmail.com>
2019-03-25 00:31:54 -0700

    Create _PageController.cs

---
+++ ATDesktopWin/Controllers/PageControllers/_PageController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATDesktopWin.Controllers.PageControllers
+{
+    public abstract class _PageController
+    {
+        protected _Controller viewController;
+        
+        protected abstract Object PageData { get; }
+
+        public _PageController(_Controller parent)
+        {
+            viewController = parent;
+        }
+
+        /// <summary>
+        /// Show page and indicate loading....
+        /// </summary>
+        protected abstract void ShowPage();
+
+        /// <summary>
+        /// Represent page data and stop loading indication.
+        /// </summary>
+        protected abstract void RepresentPageData();
+
+        protected abstract void LoadPageData(Action actionOnSuccess);
+
+        public void Refresh(bool force = false)
+        {
+            ShowPage();
+
+            if (force || PageData == null)
+            {
+                LoadPageData(RepresentPageData);
+            } else
+            {
+                RepresentPageData();
+            }
+        }
+
+        internal static _PageController Create(HomeController.Page page)
+        {
+            HomeController parent = AppManager.Instance.GetCurController<HomeController>();
+            switch (page)
+            {
+                case HomeController.Page.DASHBOARD:
+                    return new DashboardPageController(parent);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
