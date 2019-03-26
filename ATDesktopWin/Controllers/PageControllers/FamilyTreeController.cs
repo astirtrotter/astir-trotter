b155067daeb51badfb0d5b454b898ac08993702c
Pro Dev <prodev9999@gmail.com>
2019-03-26 07:29:53 -0700

    Added family tree controller

---
+++ ATDesktopWin/Controllers/PageControllers/FamilyTreeController.cs
@@ -0,0 +1,44 @@
+﻿using ATCommon.Global;
+using ATDesktopWin.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATDesktopWin.Controllers.PageControllers
+{
+    public class FamilyTreeController : _PageController
+    {
+        public FamilyTreeController(_Controller parent) : base(parent)
+        {
+            
+        }
+
+        protected override Object PageData {
+            get => null;
+        }
+
+        internal override string Title => Constants.DASHBOARD;
+
+        protected override void LoadPageData(Action actionOnSuccess)
+        {
+            // TODO
+
+            actionOnSuccess();
+        }
+
+        protected new void RepresentPageData()
+        {
+            // TODO
+
+            base.RepresentPageData();
+        }
+
+        protected new void ShowPage()
+        {
+            base.ShowPage();
+            ((HomeView)viewController.View).ShowPage(HomeController.Page.FAMILY_TREE);
+        }
+    }
+}
