b7f0d365804c97dc4ffcf045ed70885886815f36
Pro Dev <prodev9999@gmail.com>
2019-03-26 02:02:06 -0700

    Created relationship model

---
+++ ATWeb/models/_relationship.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Op = require('sequelize').Op;
+    const Relationship = sequelize.define('Relationship',
+        {
+            relation: { type: DataTypes.STRING(50), allowNull: false, primaryKey: true },
+            src: { type: DataTypes.STRING(50), allowNull: false },
+            dst: { type: DataTypes.STRING(50), allowNull: false }
+        }, {
+            timestamps: false,
+            scopes: {
+                byRelation: function (relation) { return { where: { relation: { [Op.like]: relation } }, limit: 1 }; }
+            }
+        });
+
+    return Relationship;
+};
