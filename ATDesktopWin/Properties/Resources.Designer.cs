61646b9c81d78d355409375221dfca713dcab906
Pro Dev <prodev9999@gmail.com>
2019-03-28 14:57:01 +0800

    Added leaf resource

--- ATDesktopWin/Properties/Resources.Designer.cs
+++ ATDesktopWin/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace ATDesktopWin.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap leaf {
+            get {
+                object obj = ResourceManager.GetObject("leaf", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
